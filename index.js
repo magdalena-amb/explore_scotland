0271198bbe57ec6527e575d370872e94ab1e4d2a
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2021-01-21 15:22:25 +0100

    Add review delete middleware to campground model

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 app.get('/campgrounds/:id', catchAsync(async (req,res)=>{
     const campground = await Campground.findById(req.params.id).populate('reviews');
-    console.log(campground);
     res.render('campgrounds/show', {campground});
 }));
 
