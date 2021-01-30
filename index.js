1fe307c58a273348ed5c38bdd84ac0af924cc4ae
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2021-01-30 20:01:22 +0100

    Set up passport in index.js

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+const { getMaxListeners } = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/camplife', {
    useNewUrlParser: true,
@@ -47,10 +53,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+});
+
+app.get('/fakeUser', async (req, res) => {
+const user = new User({email: 'magda@gmail.com', username: 'Magda' });
+const newUser = await User.register(user, 'mypassword');
+res.send(newUser);
 });
 
 app.use('/campgrounds', campgrounds);
