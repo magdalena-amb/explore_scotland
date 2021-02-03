6827f5c656bb7347e1085a94eaf0ff69078ed691
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2021-02-03 15:26:36 +0100

    Set up cloudinary storage

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,8 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const {index, renderNewForm, addNewCampground, showCampground, renderEditForm, updateCampground, deleteCampground} = require('../controllers/campgrounds');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'}); 
+const { storage } = require('../cloudinary');
+const upload = multer({storage}); 
 
 
 router.route('/')
