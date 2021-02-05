dc631faa1d697e428acf0e127e892b78c9a4a7c7
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2021-02-05 13:57:05 +0100

    Add basic map marker

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,9 +1,12 @@
-<script>
-      mapboxgl.accessToken = mapToken;
-      const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
-      });
-</script>
+
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+center: campground.geometry.coordinates, // starting position [lng, lat]
+zoom: 11 // starting zoom
+});
+
+new mapboxgl.Marker()
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
