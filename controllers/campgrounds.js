d6587fadc6cd483a18b07e405aa63adb0d85c184
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2021-02-05 12:44:39 +0100

    Add geometry to campgroundSchema

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -20,13 +20,15 @@ module.exports.addNewCampground = async (req, res, next) =>{
         query: req.body.campground.location,
         limit: 1
     }).send();
-    res.send(geoData.body.features[0].geometry.coordinates);
-    // const campground = new Campground(req.body.campground);
-        // campground.images = req.files.map(f => ({url: f.path, filename: f.filename }));  
-        // campground.author = req.user._id;
-        // await campground.save();
-        // req.flash('success', 'Successfully made new campground!');
-        // res.redirect(`/campgrounds/${campground._id}`);  
+    
+    const campground = new Campground(req.body.campground);
+        campground.geometry = geoData.body.features[0].geometry;
+        campground.images = req.files.map(f => ({url: f.path, filename: f.filename }));  
+        campground.author = req.user._id;
+        await campground.save();
+        console.log(campground);
+        req.flash('success', 'Successfully made new campground!');
+        res.redirect(`/campgrounds/${campground._id}`);  
 }
 
 module.exports.showCampground = async (req,res)=>{
